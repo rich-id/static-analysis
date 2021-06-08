3112210a82a3a9d468680793a0b9e342e5c6b846
Matthias Devlamynck <matthias.devlamynck@rich-id.fr>
2021-06-08 14:49:29 +0200

    Use post style for increment

--- configs/php-cs-fixer.dist.php
+++ configs/php-cs-fixer.dist.php
@@ -16,38 +16,37 @@ $finder = (new PhpCsFixer\Finder())
     ->exclude('vendor');
 
 return (new PhpCsFixer\Config())
-    ->setRules(
-        [
-            '@Symfony'                        => true,
-            '@PSR2'                           => true,
-            'braces'                          => [
-                'position_after_functions_and_oop_constructs' => 'next',
+    ->setRules([
+        '@Symfony'                        => true,
+        '@PSR2'                           => true,
+        'braces'                          => [
+            'position_after_functions_and_oop_constructs' => 'next',
+        ],
+        'binary_operator_spaces'          => [
+            'operators' => [
+                '=>' => 'align_single_space_minimal',
             ],
-            'binary_operator_spaces'          => [
-                'operators' => [
-                    '=>' => 'align_single_space_minimal',
-                ],
-            ],
-            'concat_space'                    => ['spacing' => 'one'],
-            'declare_strict_types'            => true,
-            'native_function_invocation'      => [
-                'include' => ['@internal'],
-                'scope' => 'namespaced',
-                'strict' => true,
-            ],
-            'no_null_property_initialization' => true,
-            'no_superfluous_elseif'           => true,
-            'no_useless_else'                 => true,
-            'phpdoc_line_span'                => [
-                'property' => 'single',
-                'const'    => 'single',
-                'method'   => 'single',
-            ],
-            'simplified_if_return'            => true,
-            'single_line_throw'               => false,
-            'void_return'                     => true,
-            'yoda_style'                      => false,
-        ]
-    )
+        ],
+        'concat_space'                    => ['spacing' => 'one'],
+        'declare_strict_types'            => true,
+        'increment_style'                 => ['style' => 'post'],
+        'native_function_invocation'      => [
+            'include' => ['@internal'],
+            'scope'   => 'namespaced',
+            'strict'  => true,
+        ],
+        'no_null_property_initialization' => true,
+        'no_superfluous_elseif'           => true,
+        'no_useless_else'                 => true,
+        'phpdoc_line_span'                => [
+            'property' => 'single',
+            'const'    => 'single',
+            'method'   => 'single',
+        ],
+        'simplified_if_return'            => true,
+        'single_line_throw'               => false,
+        'void_return'                     => true,
+        'yoda_style'                      => false,
+    ])
     ->setFinder($finder)
     ->setRiskyAllowed(true);
